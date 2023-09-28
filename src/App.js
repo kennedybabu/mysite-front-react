b5c3050f30f1209f8ada6f20bfe3ed33535a39d6
kennedybabu <kennedynjogu8@gmail.com>
2023-09-28 08:42:44 +0300

    feature: style the landingpage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="container flex flex-row">
+
+          <div className="sidebar min-w-[350px]">
+            <div className="flex flex-column w-full">
+              <div className="top min-h-[407px] pt-[50px] bg-[#1d3557] relative w-full">
+                  <h2 className='text-2xl text-center'>beehive</h2>
+
+                  <div className="absolute py-8 px-4 text-center text-black inset-x-0 shadow-xl -bottom-[20%] h-[250px] aspect-square bg-stone-100 rounded-sm w-[70%] mx-auto">
+                      <div className='h-[50px] bg-red-200 mx-auto rounded-full aspect-square'>
+
+                      </div>
+                      <h4 className='font-bold'>Rahmi Cooper</h4>
+                      <small>Member</small>
+                      <hr />
+                      <div className='flex flex-row gap-5 mt-3 items-center justify-center'>
+                        <div>
+                            <p>0</p>
+                            <small>Friends</small>
+                        </div>
+
+                        <div>
+                            <p>2</p>
+                            <small>Groups</small>
+                        </div>
+                      </div>
+                  </div>
+              </div>
+
+              <div className="bottom">
+
+              </div>
+            </div>
+          </div>
+
+          <div className="main-content w-full">
+              <Header /> 
+              <div className='flex flex-row h-screen mt-[80px]'>
+                  <div className="basis-3/4 h-full h-full border-r border-gray-100">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, sunt.
+                  </div>
+
+                  <div className="basis-1/4">
+                    sidebar
+                  </div>
+              </div>
+          </div>
+        </div>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { FaSistrix, FaUserCircle, FaRegBell } from 'react-icons/fa';
+import {HiOutlineMailOpen} from "react-icons/hi"
+import {BsBag} from "react-icons/bs"
+
+
+
+const Header = () => {
+  return (
+    <div className='p-2 fixed top-0 z-20 bg-white border-b border-gray-100 grid justify-items-start h-[80px] p-2 w-full'>
+         <div className="flex flex-row items-center px-2 justify-between w-full">
+            <div className='flex items-center'>
+                < FaSistrix />
+                <input type="text" placeholder='search' className='align-left mx-2'/>
+            </div>
+
+            <div className='flex flex-row'>
+                <div className='p-1 h-[30px] mx-2 cursor-pointer bg-gray-100 rounded-full flex items-center aspect-square justify-center'>
+                    < FaUserCircle />
+                </div>
+
+                <div className='p-1 h-[30px] mx-2 cursor-pointer bg-gray-100 rounded-full flex items-center aspect-square justify-center'>
+                    < FaRegBell />
+                </div>
+
+                <div className='p-1 h-[30px] mx-2 cursor-pointer bg-gray-100 rounded-full flex items-center aspect-square justify-center'>
+                    < HiOutlineMailOpen />
+                </div>
+
+                <div className='p-1 h-[30px] mx-2 cursor-pointer bg-gray-100 rounded-full flex items-center aspect-square justify-center'>
+                    < BsBag />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
